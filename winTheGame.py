7e64214971409ab11efddca184ad243fc8e82049
Mikkel Neiiendam <mikklelamstrup@gmail.com>
2025-02-16 19:57:14 +0100

    timer incoorporated, and a win sequnce

--- winTheGame.py
+++ winTheGame.py
@@ -1,5 +1,6 @@
 import pygame 
 import random
+import time
 
 pygame.init()
 
@@ -18,10 +19,14 @@ speed = 5
 obstacleX = random.randint(0, WIDTH - 50)
 obstacleY = random.randint(0, HEIGHT - 50)
 obstacleSpeedY = 4
+
 font = pygame.font.SysFont("Arial", 50)
+
+timeStart = time.time()
 
 running = True
 gameOver = False
+win = False
 
 while running:
     pygame.time.delay(30)
@@ -30,7 +35,7 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
-    if not gameOver:
+    if not gameOver and not win:
 
         keys = pygame.key.get_pressed()
         if keys[pygame.K_LEFT]: x -= speed
@@ -49,6 +54,10 @@ while running:
         if playerRect.colliderect(obstacleRect):
             gameOver = True
 
+        if x >= WIDTH - 50:
+            win = True
+            elapseTime = round(time.time() - timeStart, 2)
+
     screen.fill(WHITE)
     pygame.draw.rect(screen, BLUE, (x, y, 50, 20))
     pygame.draw.rect(screen, RED, (obstacleX, obstacleY, 50, 50))
@@ -57,7 +66,14 @@ while running:
         gameOverText = font.render("GAME OVER", True, BLACK)
         screen.blit(gameOverText, (WIDTH // 2 - 100, HEIGHT // 2 - 25))
         pygame.display.update()
+        pygame.time.delay(5000)
+        running = False
+        
 
+    if win:
+        winText = font.render(f"YOU WIN IN {elapseTime} SECONDS!", True, BLACK)
+        screen.blit(winText, (WIDTH // 2 - 200, HEIGHT // 2 - 25))
+        pygame.display.update()
         pygame.time.delay(5000)
         running = False
     
