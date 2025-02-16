9d433559d25c7699f483b5b7c4177c1eb88708cd
Mikkel Neiiendam <mikklelamstrup@gmail.com>
2025-02-16 13:45:14 +0100

    first commit

---
+++ first.py
@@ -0,0 +1,34 @@
+import pygame 
+
+pygame.init()
+
+WIDTH, HEIGHT = 500, 500
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("My first game")
+
+WHITE = (255, 255, 255)
+BLUE = (0, 0, 255)
+
+x, y = 225, 225 
+speed = 5
+
+running = True
+while running:
+    pygame.time.delay(30)
+
+for event in pygame.event.get():
+    if event.type == pygame.QUIT:
+        running = False
+
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_LEFT]: x -= speed
+    if keys[pygame.K_RIGHT]: x += speed
+    if keys[pygame.K_UP]: y -= speed
+    if keys[pygame.K_DOWN]: y += speed
+
+screen.fill(WHITE)
+pygame.draw.rect(screen, BLUE, (x, y, 50, 50))
+pygame.display.update()
+
+pygame.quit()
+
